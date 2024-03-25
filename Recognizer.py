09d7f8e03106b0392aab4ea5b043f2c7e08802bb
John-S-Reddick <john.work.reddick@outlook.com>
2024-03-25 19:18:59 -0400

    partial work

--- Recognizer.py
+++ Recognizer.py
@@ -3,13 +3,6 @@ import mediapipe as mp
 import numpy as np
 import pandas as pd
 import time
-import DataManagement as dm
-import webbrowser
-import PIL.ImageGrab
-import keyboard
-import pyautogui
-import win32con
-import win32api
 
 class ASCL(self):
     def __init__(self, actions):
@@ -28,4 +21,22 @@ class ASCL(self):
         self.template.s3 = 0
 
         pts = angular_dp(samplePts, epsilon)
-        N = 
+        N = |pts|
+
+        for i, elem, in enumerate(N):
+            elem.startFrame = -1
+            elem.endFrame = -1
+            elem.cumulativeCost = 0
+            elem.cumulativeLengt = 0
+            elem.score = np.inf
+            if (i == 0):
+                elem.score = ((1-np.cos(np.linspace(0,2*np.pi,301)))**2)
+            
+            #Push-Back(template.row[0], elem)
+            #Push-Back(template.row[1], elem)
+                
+            #Ask corey what this means (potentially is a template using the first and secoond row of matrix data)
+                
+            if (i > 0):
+                vec1 = np.array(pts[i] - pts[i - 1])
+                #Push-Back(template.T, )
