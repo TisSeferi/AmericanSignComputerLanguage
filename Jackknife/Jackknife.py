2eff2fe15431c627fa60ec42d87c50c73957e6e5
Ertis Seferi <n01492932@unf.edu>
2024-03-30 15:59:36 -0400

    neworder

--- Jackknife/Jackknife.py
+++ Jackknife/Jackknife.py
@@ -20,3 +20,44 @@ class Jackknife:
 
 
                 #Line 72
+
+class JackknifeTemplate:
+    def __init__(self, blades, sample):
+        self.sample = sample
+        self.gesture_id = sample.gesture_id
+        self.lower = []
+        self.upper = []
+
+        self.lb -1.0
+        self.cf = 1.0
+
+        self.rejection_threshold = np.inf()
+
+        self.features = JackknifeFeatures(blades, self.trajectory)
+
+class JackknifeFeatures:
+    def __init__(self, blades, points):
+        self.pts = blades
+        self.pts = points
+        self.pts = resample(self.pts, blades.resample_cnt)
+        
+        self.vecs = []
+        self.abs = np.zeros(points.shape[1])
+        minimum = Vector(self.pts[0])
+        maximum = Vector(self.pts[0])
+
+        for i in range (1, len(self.pts)):
+            vec = self.pts[i] - self.pts[i-1]
+
+            self.abs += np.a
+        
+
+
+
+
+
+class Distributions:
+    def __init__(self, max_score, bin_cnt):
+        self.neg = Vector(0, bin_cnt)
+        self.pos = Vector(0, bin_cnt)
+        self.max_score = max_score
