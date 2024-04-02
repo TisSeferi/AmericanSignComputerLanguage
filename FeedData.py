a0e0550676c5ee92a676872541bc15b5f822f3e0
John-S-Reddick <154930148+John-S-Reddick@users.noreply.github.com>
2024-04-02 10:21:56 -0400

    Retooled FeedData

--- FeedData.py
+++ FeedData.py
@@ -4,40 +4,39 @@ import numpy as np
 import pandas as pd
 import time
 import os
+import Jackknife as jk
+
 USE_DATAFRAMES = False
 NUM_POINTS = 21
 
-BUFFER_WINDOW = 10 #In seconds
-BUFFER_FPS = 30    #TODO Fix for variable framerate cameras
+CV2_RESIZE = (640, 480)
+
+BUFFER_WINDOW = 10  # In seconds
+BUFFER_FPS = 30  # TODO Fix for variable framerate cameras
 BUFFER_FRAMES = BUFFER_WINDOW * BUFFER_FPS
 
 BUFFER_LENGTH = BUFFER_FRAMES * NUM_POINTS
 
-RAW_VIDS_FOLDER = 'TestVideos'
-TEMPLATES = 'templates'
-
-
-
-
-
+RAW_VIDS_FOLDER = 'TestVideos/'
+TEMPLATES = 'templates/'
 
 HAND_REF = [
-        'wrist',
-        'thumb_cmc', 'thumb_mcp', 'thumb_ip', 'thumb_tip',
-        'index_finger_mcp', 'index_finger_pip', 'index_finger_dip', 'index_finger_tip',
-        'middle_finger_mcp', 'middle_finger_pip', 'middle_finger_dip', 'middle_finger_tip',
-        'ring_finger_mcp', 'ring_finger_pip', 'ring_finger_dip', 'ring_finger_tip',
-        'pinky_mcp', 'pinky_pip', 'pinky_dip', 'pinky_tip',
-    ]
+    'wrist',
+    'thumb_cmc', 'thumb_mcp', 'thumb_ip', 'thumb_tip',
+    'index_finger_mcp', 'index_finger_pip', 'index_finger_dip', 'index_finger_tip',
+    'middle_finger_mcp', 'middle_finger_pip', 'middle_finger_dip', 'middle_finger_tip',
+    'ring_finger_mcp', 'ring_finger_pip', 'ring_finger_dip', 'ring_finger_tip',
+    'pinky_mcp', 'pinky_pip', 'pinky_dip', 'pinky_tip',
+]
 
-def Euclidean_Dist(self, df1, df2, cols=['x', 'y']):
+
+def euclidean_dist(self, df1, df2, cols=['x', 'y']):
     return np.linalg.norm(df1[cols].values - df2[cols].values, axis=1)
 
-def Process_Video(videoName):
+
+def process_video(video_name):
     t = time.time()
-    cap = cv2.VideoCapture(videoName)
-    name = videoName.split('.')[0] + '-' + videoName.split('.')[1]
-    name = name.split('/')[1]
+    cap = cv2.VideoCapture(video_name)
 
     duration = cap.get(cv2.CAP_PROP_POS_MSEC)
 
@@ -45,61 +44,51 @@ def Process_Video(videoName):
         print("Error: Failed to open File.")
         exit()
 
-    #print("HandDetector initialized successfully.")
+    # print("HandDetector initialized successfully.")
 
     hands = mp.solutions.hands.Hands()
-    
-    #The list for returning the dataframes
-    data =[]
+
+    # The list for returning the dataframes
+    data = []
     success, img = cap.read()
-    while(success): 
+    while success:
         if not success:
             print("Error in reading!")
             cap.release()
             exit()
 
-        #Image resizing for standardiztion
-        img = cv2.resize(img, (640, 480))
+        # Image resizing for standardiztion
+        img = cv2.resize(img, CV2_RESIZE)
 
-        #Running recognition
+        # Running recognition
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = hands.process(imgRGB)
 
-        #Extracting Landmarks
+        # Extracting Landmarks
         if results.multi_hand_landmarks:
             landmarks = [results.multi_hand_landmarks[0]]
             if landmarks:
                 for handLms in landmarks:
-                    #Convert landmarks to dataframe
+                    # Convert landmarks to dataframe
                     points = handLms.landmark
                     d = np.zeros((NUM_POINTS, 2))
                     for id, lm in enumerate(points):
                         d[id][0] = lm.x
                         d[id][1] = lm.y
 
-                    if(USE_DATAFRAMES):
-                        d = pd.DataFrame(data=d, columns=['x', 'y'], index=HAND_REF)
-
                     data.append(d)
-        
 
         success, img = cap.read()
 
     cap.release()
     cv2.destroyAllWindows()
 
-
-    if(USE_DATAFRAMES):
-        data = pd.concat(data)
-        data.to_csv('out.csv')
-    else:        
-        np.save(TEMPLATES + name, np.vstack(data))
-    t = time.time() - t
-    #print('Elapsed Time: ' + "%.2f" % t)
     return data
 
-def Live_Process():
+
+def live_process():
     cap = cv2.VideoCapture(0)
+    recognizer = jk()
 
     if not cap.isOpened():
         print("Error: Failed to open File.")
@@ -108,66 +97,73 @@ def Live_Process():
     print("HandDetector initialized successfully.")
 
     hands = mp.solutions.hands.Hands()
-    
-    #The list for returning the dataframes
+
+    # The list for returning the dataframes
     data = np.zeros((BUFFER_LENGTH, 2))
     frame = 0
 
     success, img = cap.read()
-    while(success): 
+    while success:
         if not success:
             print("Error in reading!")
             cap.release()
             exit()
 
-        #Image resizing for standardiztion
-        img = cv2.resize(img, (640, 480))
+        # Image resizing for standardiztion
+        img = cv2.resize(img, CV2_RESIZE)
 
-        #Running recognition
+        # Running recognition
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = hands.process(imgRGB)
 
-        #Extracting Landmarks
+        # Extracting Landmarks
         if results.multi_hand_landmarks:
             landmarks = [results.multi_hand_landmarks[0]]
             if landmarks:
                 for handLms in landmarks:
-                    #Convert landmarks to dataframe
+                    # Convert landmarks to dataframe
                     points = handLms.landmark
-                    for id, lm in enumerate(points):
-                        data[frame * NUM_POINTS + id][0] = lm.x
-                        data[frame * NUM_POINTS + id][1] = lm.y                   
+                    for ii, lm in enumerate(points):
+                        data[frame * NUM_POINTS + ii][0] = lm.x
+                        data[frame * NUM_POINTS + ii][1] = lm.y
         else:
-            for id in range(NUM_POINTS):
-                data[frame * NUM_POINTS + id][0] = -1
-                data[frame * NUM_POINTS + id][1] = -1
+            for ii in range(NUM_POINTS):
+                data[frame * NUM_POINTS + ii][0] = -1
+                data[frame * NUM_POINTS + ii][1] = -1
 
         frame = (frame + 1) % BUFFER_FRAMES
-        print(data)
-        success, img = cap.read()
+        recognizer.classify(data)
 
+        success, img = cap.read()
 
     cap.release()
     cv2.destroyAllWindows()
 
+
 # Web processing test
 # Process_Video('https://www.pexels.com/download/video/3959694/')
-    
-def Extract_From_Vids():
-    for str in os.listdir(RAW_VIDS_FOLDER):
-        str = RAW_VIDS_FOLDER + str
-        Process_Video(str)
-        #TODO Move np.save here
+def save_template(path):
+    name = path.split('.')[0] + '-' + path.split('.')[1]
+    path = RAW_VIDS_FOLDER + path
+    data = process_video(path)
+    np.save(TEMPLATES + name, data)
+    print(data)
 
 
-def Assemble_Templates():
-    list = []
-    for str in os.listdir(TEMPLATES):
-        list.append(np.load(TEMPLATES + '/' + str))
+def extract_from_videos():
+    for path in os.listdir(RAW_VIDS_FOLDER):
+        save_template(path)
 
-    return list
 
-Live_Process()
+def assemble_templates():
+    templates = []
+    for path in os.listdir(TEMPLATES):
+        templates.append(np.load(TEMPLATES + '/' + path))
+
+    return templates
+
+
+extract_from_videos()
 # print('Running Recognition')
 # 
 # t = time.time()
--- Jackknife/Jackknife.py
+++ Jackknife/Jackknife.py
@@ -1,25 +1,28 @@
 import FeedData as fd
+import numpy as np
+import JkBlades
+import Vector
 
-#Add "JackknifeTemplate" object with parameters "blades" and "sample"
-#Add "JackknifeFeatures" with parameters "blades" and "trajectory"
-#Terms:
-    #Trajectory is the incoming data stream from our camera feed
+
+# Add "JackknifeTemplate" object with parameters "blades" and "sample"
+# Add "JackknifeFeatures" with parameters "blades" and "trajectory"
+# Terms:
+# Trajectory is the incoming data stream from our camera feed
 
 class Jackknife:
-    def __init__(self, blades, templates):
+    def __init__(self, blades=JkBlades(), templates=fd.assemble_templates()):
         self.blades = blades
-        self.templates = fd.Assemble_Templates()
-            
+        self.templates = templates
 
-        def classify(self, trajectory):
-            features = JackknifeFeatures(this.blades, trajectory)
-            template_cnt = self.templates.len()
+    def classify(self, trajectory):
+        features = JackknifeFeatures(self.blades, trajectory)
+        template_cnt = self.templates.len()
 
-            for tt, template, in enumerate(self.templates):
-                cf = 1.0
+        for tt, template, in enumerate(self.templates):
+            cf = 1.0
 
+            # Line 72
 
-                #Line 72
 
 class JackknifeTemplate:
     def __init__(self, blades, sample):
@@ -28,32 +31,29 @@ class JackknifeTemplate:
         self.lower = []
         self.upper = []
 
-        self.lb -1.0
+        self.lb - 1.0
         self.cf = 1.0
 
         self.rejection_threshold = np.inf()
 
         self.features = JackknifeFeatures(blades, self.trajectory)
 
+
 class JackknifeFeatures:
-    def __init__(self, blades, points):
+    def __init__(self, blades=JkBlades, points=None):
         self.pts = blades
         self.pts = points
         self.pts = resample(self.pts, blades.resample_cnt)
-        
+
         self.vecs = []
         self.abs = np.zeros(points.shape[1])
         minimum = Vector(self.pts[0])
         maximum = Vector(self.pts[0])
 
-        for i in range (1, len(self.pts)):
-            vec = self.pts[i] - self.pts[i-1]
+        for i in range(1, len(self.pts)):
+            vec = self.pts[i] - self.pts[i - 1]
 
             self.abs += np.a
-        
-
-
-
 
 
 class Distributions:
