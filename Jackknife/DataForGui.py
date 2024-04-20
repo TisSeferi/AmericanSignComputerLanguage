315980b4b759bb0f4ed9e67b477391a1a93abd57
John-S-Reddick <john.work.reddick@outlook.com>
2024-04-20 16:47:40 -0400

    Added times.npy

--- Jackknife/DataForGui.py
+++ Jackknife/DataForGui.py
@@ -29,8 +29,6 @@ HAND_REF = [
     'ring_finger_mcp', 'ring_finger_pip', 'ring_finger_dip', 'ring_finger_tip',
     'pinky_mcp', 'pinky_pip', 'pinky_dip', 'pinky_tip',
 ]
-
-
 def capture_vid(video_name=1):
     spot = PRE_RECORDED_BOOT_TIME
 
