a69431515bce07c597b071ac4ce8cf5a9eabe61c
Ertis Seferi <n01492932@unf.edu>
2024-04-20 17:01:46 -0400

    thingy

--- Jackknife/TemplateGui.py
+++ Jackknife/TemplateGui.py
@@ -5,20 +5,20 @@ import cv2
 from PIL import Image, ImageTk
 
 def append_to_console(message):
-    console_output.configure(state='normal')  # Temporarily make the widget writable
-    console_output.insert(tk.END, message + '\n')  # Add text
-    console_output.configure(state='disabled')  # Disable editing again
+    console_output.configure(state='normal')
+    console_output.insert(tk.END, message + '\n')  
+    console_output.configure(state='disabled')  
     console_output.see(tk.END)
 
 def update_frame():
-    ret, cv_image = cap.read()  # Capture frame-by-frame
+    ret, cv_image = cap.read()
     if ret:
-        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)  # Convert from BGR to RGB
+        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB) 
         pil_image = Image.fromarray(cv_image)
         tk_image = ImageTk.PhotoImage(image=pil_image)
-        canvas.image = tk_image  # Keep a reference, prevent garbage-collection
+        canvas.image = tk_image 
         canvas.itemconfig(image_on_canvas, image=tk_image)
-    main.after(10, update_frame)  # Call this function again in 10 ms
+    main.after(10, update_frame) 
 
 main = tk.Tk()
 main.title('ASCL Prototype')
@@ -28,7 +28,7 @@ title_label = ttk.Label(main, text='ASCL', font='Calibri 24 bold')
 title_label.pack(pady=10)
 
 canvas = tk.Canvas(main, width=400, height=400)
-canvas.pack(side=tk.LEFT, padx=20, pady=10)  # Aligned to the left with padding
+canvas.pack(side=tk.LEFT, padx=20, pady=10)
 image_on_canvas = canvas.create_image(200, 200, anchor='center')
 
 record_frame = ttk.Frame(main)
@@ -56,14 +56,14 @@ live_button = ttk.Button(live_frame, text='Live')
 live_button.pack(side=tk.RIGHT, padx=10)
 
 console_output = scrolledtext.ScrolledText(main, height=16)
-console_output.pack(side=tk.BOTTOM, fill=tk.X, padx=10, pady=(0, 20))  # Fill horizontally
+console_output.pack(side=tk.BOTTOM, fill=tk.X, padx=10, pady=(0, 20))
 
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     append_to_console("Failed to open camera.")
     raise ValueError("Unable to open video source")
 
-update_frame()  # Start the camera update loop
+update_frame()
 
 main.mainloop()
 
